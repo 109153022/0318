0e307adb8ad6c818364061392d543643d3a02982
109153022 <162954082+109153022@users.noreply.github.com>
2024-03-18 16:01:56 +0800

    0318 單位換算撰寫
    程式快撰寫完成，差清除功能

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douCM;
+
+            douCM = Convert.ToDouble(txtCM.Text);
+
+            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+            txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", douCM / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", douCM / 30.48);
+            txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
+            txtYard.Text = string.Format("{0:0.##########}", douCM / 91.44);
+
+        }
+    }
+}
